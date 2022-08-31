3b9f53314a0648d7a50b4c8c8367e0d7d2ad5e3c
William Tavares <willtaavares@gmail.com>
2022-08-31 19:19:03 -0300

    feat: import routes and app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import { BotsProvider } from "./context/bots.context";
 import AppRoutes from "./routes";
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <AppRoutes />
+  <ChakraProvider theme={theme} resetCSS>
+    <BotsProvider>
+      <AppRoutes />
+    </BotsProvider>
   </ChakraProvider>
 );
